6d1389ef699d0f2eebea9a3739bbc3e24784a593
Dilip Nayak <dilipnayak7652@gmail.com>
2023-12-29 21:55:47 +0530

    social media

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 function SignupForm() {
+  const formSchema = z.object({
+    username: z.string().min(2).max(50),
+  });
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  const handleSignup = async ()=>{
+    
+  }
   return (
-    <div>SignupForm</div>
-  )
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSignup)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
 
-export default SignupForm
+export default SignupForm;
